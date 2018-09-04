816fe9d3afdfcd6597180157d4b9b4dee8e895e7
iabloko <31312961+iabloko@users.noreply.github.com>
2018-09-04 17:50:21 +0300

    Pull#del

---
+++ LEDStripLight/Assets/LightBoard/Code/LEDstrip_Cupol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LEDstrip_Cupol : MonoBehaviour {
+
+	[System.Serializable]
+	public class VVertical : System.Object {
+		public MeshRenderer[] _VMesh;
+	}
+
+	[SerializeField] private List<VVertical> _VVertical;
+	[SerializeField] private Color[] _Color;
+	[SerializeField] private Material _LightOn, _LightOff;
+}
